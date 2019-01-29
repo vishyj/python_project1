af38af234da2cde9f6639debfffb73c0c36072f7
vishyj <vishy.j@outlook.com>
2019-01-28 21:56:46 -0600

    no message

--- factors.py
+++ factors.py
@@ -13,4 +13,5 @@ if x % 5==0  and  x % 2==0:
     print ("Your number is divisible by 2 and 5!!")
 if x % 5==0 and x % 7==0:
     print ("Your number is divisible by 7 and 5!!")
-    # this is vishal 
+    # this is vishal
+    # Hello Vishal
